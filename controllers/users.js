04c150047457a44aff4b6af59885ef4b5fed6bf2
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-07-07 01:18:00 +0100

    completed signup flow and added partial nav to index

--- controllers/users.js
+++ controllers/users.js
@@ -5,8 +5,20 @@ const User = require('../models/users');
 
 userRouter.get('/new', (req, res) => {
     res.render('users/new.ejs',  {
-
+        // currentUser = req.session.currentUser
     });
-})
+});
+
+userRouter.post('/', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(15));
+    User.create(req.body, (err, newUser) => {
+        if (err) {
+            res.send(err);
+            console.log(err);
+        } else {
+            res.redirect('/feature-requests');
+        }
+    });
+});
 
 module.exports = userRouter;
