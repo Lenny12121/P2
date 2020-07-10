094a33fc825d84fb0c118c7f3d10d78416293f23
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-07-10 17:13:21 +0100

    completed preliminary downvote logic

--- controllers/features.js
+++ controllers/features.js
@@ -148,34 +148,6 @@ router.post('/', (req, res) =>  {
 });
 });
 
-
-
-// router.post('/', (req, res) => {
-//     // 'profile_pic' is the name of our file input field in the HTML form
-//     let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('image');
-
-//     upload(req, res, function(err) {
-//         // req.file contains information of uploaded file
-//         // req.body contains information of text fields, if there were any
-
-//         if (req.fileValidationError) {
-//             return res.send(req.fileValidationError);
-//         }
-//         else if (!req.file) {
-//             return res.send('Please select an image to upload');
-//         }
-//         else if (err instanceof multer.MulterError) {
-//             return res.send(err);
-//         }
-//         else if (err) {
-//             return res.send(err);
-//         }
-
-//         // Display uploaded image for user validation
-//         res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
-//     });
-// });
-
 //Upvote/downvote adapted from here: https://github.com/Binayak07/rentomojo_assignment/blob/master/app.js
 //UPVOTE/DOWNVOTE
 router.post("/upvoted", (req,res) => {
@@ -232,11 +204,6 @@ router.post("/downvoted", (req, res) => {
         })
     });
 });
-
-
-
-
-
 
 // Show Route
 router.get('/:company/:id', (req, res) =>  {
